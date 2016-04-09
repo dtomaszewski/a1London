6d1df476468dea4196855777806762f67e225f5d
dtomaszewski.js@gmail.com <dtomaszewski.js@gmail.com>
2016-04-09 15:21:04 +0200

    Bikepoints displayed

--- pages/map-page/mapPageCtrl.js
+++ pages/map-page/mapPageCtrl.js
@@ -1,7 +1,17 @@
 'use strict';
 class MapPageCtrl {
     constructor(LondonTransport) {
-        this.test = 'HALO HALO TU LONDYN!!';
+        LondonTransport.all('BikePoint').getList().then((results) => {
+            this.bikePoints = [];
+
+            results.forEach((result) => {
+                this.bikePoints.push({
+                    id: result.id,
+                    latitude: result.lat,
+                    longitude: result.lon
+                });
+            });
+        });
     }
 }
 
